b46a97ce80f059a1658696b7f757cfb0efbca4c0
Dianrozzz-ai <andrresrodriguez313@gmail.com>
2025-04-05 01:31:47 +0000

    PrimerCambio

---
+++ js/lista.js
@@ -0,0 +1,20 @@
+function mostrarlista(listaPokes){
+    const app = document.getElementById("app");
+    app.innerHTML = "lista";
+    let Pokes = "";
+
+    for (let i = 0; i < listaPokes.length; i++) {
+        let id = pokemones[i].url.split("/")[6];
+        Pokes += `
+        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${pokemones[i].name}')">
+            <p>#${id}</p>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${pokemones[i].name}">
+            <p>${pokemones[i].name}</p>
+        </div>`;
+    }
+    seccion.innerHTML = Pokes;
+    
+    /*app.appendChild(buscador);*/
+    app.appendChild(seccion);
+
+}
